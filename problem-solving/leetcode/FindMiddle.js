6ca6cb2075702b2cb669aed52a48fefb6b0c014d
Khaled Hamayel <1231439@student.birzeit.edu>
2026-04-26 00:07:23 +0300

    Add linked list practice solutions

---
+++ problem-solving/leetcode/FindMiddle.js
@@ -0,0 +1,29 @@
+// Middle of linked list
+// https://leetcode.com/problems/middle-of-the-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    
+    if(head === null || head.next === null){
+        return head;
+    }
+    let fast = head;
+    let slow = head;
+
+    while(fast !== null && fast.next !== null){
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+
+    return slow;
+};
